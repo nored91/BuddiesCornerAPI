4c220a5d56a8706e28803c8303267c5a5b81a184
KevinLegras$ <legraskevin@outlook.com>
2023-01-16 11:13:14 +0100

    BD-29 - Add DataBase script

---
+++ database/script.sql
@@ -0,0 +1,79 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE IF EXISTS event_user;
+DROP TABLE IF EXISTS group_user;
+DROP TABLE IF EXISTS task;
+DROP TABLE IF EXISTS "comment";
+DROP TABLE IF EXISTS "event";
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS "group";
+DROP TYPE IF EXISTS event_type;
+
+CREATE TYPE event_type AS ENUM ('other','party','sport','vacation');
+
+CREATE TABLE "group" ( 
+  group_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  title varchar(100) NOT NULL,
+  description varchar(255),
+  creation_date Date 
+);
+
+CREATE TABLE "user" ( 
+  user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  mail varchar(255) NOT NULL,
+  firstname varchar(50) NOT NULL,
+  lastname varchar(50) NOT NULL,
+  pseudo varchar(50) NOT NULL,
+  password varchar(255) NOT NULL,
+  active boolean,
+  creation_date Date NOT NULL
+);
+
+CREATE TABLE "event" ( 
+  event_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  group_id uuid,
+  title varchar(100) NOT NULL, 
+  description varchar(255),
+  creation_date Date NOT NULL,
+  event_date Date,
+  location varchar(255),
+  type event_type NOT NULL,
+  CONSTRAINT foreign_key_group_event FOREIGN KEY(group_id) REFERENCES "group"(group_id)
+);
+
+CREATE TABLE "comment" ( 
+  comment_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  event_id uuid,
+  user_id uuid,
+  message varchar(255) NOT NULL,
+  creation_date Date NOT NULL,
+  edition_date Date,
+  CONSTRAINT foreign_key_event_comment FOREIGN KEY(event_id) REFERENCES "event"(event_id),
+  CONSTRAINT foreign_key_user_comment FOREIGN KEY(user_id) REFERENCES "user"(user_id)
+);
+
+CREATE TABLE task ( 
+  task_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  event_id uuid,
+  user_id uuid,
+  title varchar(100) NOT NULL, 
+  achieve boolean,
+  CONSTRAINT foreign_key_event_task FOREIGN KEY(event_id) REFERENCES "event"(event_id),
+  CONSTRAINT foreign_key_user_task FOREIGN KEY(user_id) REFERENCES "user"(user_id)
+);
+
+CREATE TABLE event_user(
+  event_id uuid ,
+  user_id uuid,
+  PRIMARY KEY (event_id, user_id),
+  CONSTRAINT primary_key_event_event_user FOREIGN KEY (event_id) REFERENCES "event"(event_id),
+  CONSTRAINT primary_key_user_event_user FOREIGN KEY (user_id) REFERENCES "user"(user_id)
+);
+
+CREATE TABLE group_user(
+  group_id uuid,
+  user_id uuid,
+  PRIMARY KEY (group_id, user_id),
+  CONSTRAINT primary_key_group_group_user FOREIGN KEY (group_id) REFERENCES "group"(group_id),
+  CONSTRAINT primary_key_user_group_user FOREIGN KEY (user_id) REFERENCES "user"(user_id)
+);
